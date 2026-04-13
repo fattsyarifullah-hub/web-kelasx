1c9cc44f8b87e514d9a32891e427168c4a480e26
Fathan Syarifullah <syarifullahfathan@gmail.com>
2026-04-13 22:04:27 +0700

    test

--- resources/views/pengumuman/index.blade.php
+++ resources/views/pengumuman/index.blade.php
@@ -1,10 +1,17 @@
 <x-layout>
-    <h1>ini halaman pengumuman</h1>
+    <h1 class="h1-pengumuman">PENGUMUMAN</h1>
     <div>
-        @auth
-        <a href="{{ route('pengumuman.create') }}">Tambah Pengumuman</a>    
-        @endauth
-        
+        <div class="create-pengumuman">
+            @auth
+                <a href="{{ route('pengumuman.create') }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                        fill="#e3e3e3">
+                        <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+                    </svg>
+                    Tambah Pengumuman</a>
+            @endauth
+        </div>
+
         <ul>
             @foreach ($allPengumuman as $pengumuman)
                 <li>
@@ -13,12 +20,12 @@
                 </li>
                 <a href="{{ route('pengumuman.show', $pengumuman->id) }}">Lihat pengumuman</a>
                 @auth
-                <a href="{{ route('pengumuman.edit', $pengumuman->id) }}">Edit pengumuman</a>
-                <form action="{{ route('pengumuman.destroy', $pengumuman->id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Hapus pengumuman</button>
-                </form>
+                    <a href="{{ route('pengumuman.edit', $pengumuman->id) }}">Edit pengumuman</a>
+                    <form action="{{ route('pengumuman.destroy', $pengumuman->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Hapus pengumuman</button>
+                    </form>
                 @endauth
             @endforeach
         </ul>
